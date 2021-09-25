af4122480e9ee6f1d73a655e173b12483b87b6f7
Juanchoo055 <juanchoo055@gmail.com>
2021-09-25 10:59:54 -0500

    se ajusta proyecto nodejs

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const router = express.Router();
-
+const router = require('./network/routes');
 
 
 var app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended : false}));
+
+// enrutador siempre al final
+router(app);
+
+app.use('/', express.static('public'));
 
 
-
-
-app.use('/',express.static('public'))
 
 app.listen(5000);
-console.log('La aplicacion esta escuchando en http://localhost:3000')
+console.log('La aplicacion esta escuchando en http://localhost:5000')
