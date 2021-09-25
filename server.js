8ffdd92dbf17a1d66bad88ab22c7708532f2f0fb
Juanchoo055 <juanchoo055@gmail.com>
2021-09-25 10:29:43 -0500

    proyecto node.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+
+
+app.use('/',express.static('public'))
+
+app.listen(3000);
+console.log('La aplicacion esta escuchando en http://localhost:3000')
