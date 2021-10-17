efe6cf34079b0f1a566a253999cd7953439e338d
Juanchoo055 <juanchoo055@gmail.com>
2021-10-17 00:52:02 -0500

    culminacion backend users

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -16,11 +16,26 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error! ' + err))
 })
 
+router.route('/:id').get((req, res) => {
+    // using .find() without a paramter will match on all user instances
+    User.findById(req.params.id, req.body)
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json('Error! ' + err))
+})
+
 router.route('/delete/:id').delete((req, res) => {
     User.deleteOne({ _id: req.params.id })
         .then(success => res.json('Success! User deleted.'))
         .catch(err => res.status(400).json('Error! ' + err))
 })
+
+// router.put('/:id', async (req,res)=> {
+//     const {name, email, password, age} = req.body;
+//     const updateUser = {name, email, password, age};
+//     await User.findByIdAndUpdate(req.params.id, updateUser)
+//     res.json({status: 'Usuario Actualizado'})
+// })
+
 
 router.route('/update/:id').put((req, res) => {
     User.findByIdAndUpdate(req.params.id, req.body)
